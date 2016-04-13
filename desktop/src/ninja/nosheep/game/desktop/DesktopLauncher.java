cabf4d923e8f5b907233e71bb5940df138e46531
Hamsterslott <johanrasmussen94@gmail.com>
2016-04-13 18:35:45 +0200

    Initial push

---
+++ desktop/src/ninja/nosheep/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ninja.nosheep.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ninja.nosheep.game.MainController;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainController(), config);
+	}
+}
